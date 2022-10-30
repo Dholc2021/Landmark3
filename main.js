4385409f9f1ac58bc8e62ed94ec32cb9fde28c50
Daniel Holcomb <84785553+Dholc2021@users.noreply.github.com>
2022-10-29 21:55:16 -0500

    updated draft

--- main.js
+++ main.js
@@ -40,20 +40,20 @@ window.addEventListener('scroll', scrollHeader)
 /*=============== VALUE ACCORDION ===============*/
 
 
-    var acc = document.getElementsByClassName("accordion");
-    var i;
+    // var acc = document.getElementsByClassName("accordion");
+    // var i;
 
-    for (i = 0; i < acc.length; i++) {
-        acc[i].addEventListener("click", function () {
-            this.classList.toggle("active");
-            var panel = this.nextElementSibling;
-            if (panel.style.display === "block") {
-                panel.style.display = "none";
-            } else {
-                panel.style.display = "block";
-            }
-        });
-    }
+    // for (i = 0; i < acc.length; i++) {
+    //     acc[i].addEventListener("click", function () {
+    //         this.classList.toggle("active");
+    //         var panel = this.nextElementSibling;
+    //         if (panel.style.display === "block") {
+    //             panel.style.display = "none";
+    //         } else {
+    //             panel.style.display = "block";
+    //         }
+    //     });
+    // }
 
 
 
